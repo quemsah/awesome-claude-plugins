b6d6c38b01447d500ccbee64af59ce280804264f
quemsah <quemsaurose@gmail.com>
2025-12-03 23:05:38 +0200

    idk

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["http://192.168.10.68"],
+  allowedDevOrigins: ["192.168.10.68"],
 };
 
 export default nextConfig;
