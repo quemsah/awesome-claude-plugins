ebbacb109c98e07c4b15ee4b916e88158d7724da
quemsah <quemsaurose@gmail.com>
2025-12-05 11:14:57 +0200

    /

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+// const eslint = require('@eslint/js')
+// const airbnb = require('eslint-config-airbnb-extended')
+// const prettierConfig = require('eslint-config-prettier/flat')
+// const stylistic = require('@stylistic/eslint-plugin')
+// const progress = require('eslint-plugin-file-progress')
+// const promisePlugin = require('eslint-plugin-promise')
+// const unicorn = require('eslint-plugin-unicorn')
+// const tailwind = require('eslint-plugin-tailwindcss')
 
 const eslintConfig = defineConfig([
   ...nextVitals,
