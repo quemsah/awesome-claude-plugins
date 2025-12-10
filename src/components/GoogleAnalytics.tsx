b756a49c23c679145138c538c03b901a9a5a5b6d
quemsah <quemsaurose@gmail.com>
2025-12-10 12:33:02 +0200

    upd src\components\GoogleAnalytics.tsx

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,7 +3,7 @@
 import { GoogleAnalytics as NextGoogleAnalytics } from '@next/third-parties/google'
 import { GA_MEASUREMENT_ID, isAnalyticsEnabled } from '../lib/analytics.ts'
 
-export { analytics, reportWebVitals, trackEvent, trackPageView } from '../lib/analytics.ts'
+export { reportWebVitals, trackEvent, trackPageView } from '../lib/analytics.ts'
 
 export default function GoogleAnalytics() {
   if (!isAnalyticsEnabled()) {
