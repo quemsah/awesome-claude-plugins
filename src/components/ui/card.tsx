80c0340521c5b8b56dff68b08acd7f9c0e4756fa
quemsah <quemsaurose@gmail.com>
2025-12-11 20:29:04 +0200

    card init
    style
    
    header fix
    
    repo card link
    
    animate github
    
    polish
    
    style
    
    omorfo
    
    align
    
    s
    
    fix
    
    Revert "fix"
    
    This reverts commit 9db8846c6dc16378cc361194d8ce5cecc0cd7a51.
    
    fix transform origin
    
    shadcn def?

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -5,7 +5,7 @@ import { cn } from '../../lib/utils.ts'
 function Card({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
-      className={cn('bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm', className)}
+      className={cn('bg-card text-card-foreground flex flex-col gap-4 rounded-xl border py-6 shadow-sm', className)}
       data-slot="card"
       {...props}
     />
