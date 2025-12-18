71ad03321deb5abdfc1c68f0cab57c0933df7d12
quemsah <quemsaurose@gmail.com>
2025-12-19 00:02:13 +0200

    CR
    seo
    
    re-i

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -103,7 +103,7 @@ export default function StatsPage() {
     removeTitleTag(chartRef.current)
 
     return () => observer.disconnect()
-  }, [])
+  }, [chartData.length])
 
   if (loading) {
     return (
