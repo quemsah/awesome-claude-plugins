6a88b3959fbe37bc3e26d9496b6fc8cb8b87873c
quemsah <quemsaurose@gmail.com>
2026-01-03 11:13:46 +0200

    stats

--- src/schemas/stats.schema.ts
+++ src/schemas/stats.schema.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 export const StatsItemSchema = z.object({
   date: z.string().datetime(),
   size: z.string(),
-  id: z.number(),
 })
 
 export const StatsArraySchema = z.array(StatsItemSchema)
