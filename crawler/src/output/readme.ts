e49e8131405a1a5479f651af91c2e4e1fe64f89f
quemsah <quemsaurose@gmail.com>
2026-09-23 20:09:50 +0300

    style(crawler): format README escaping

--- crawler/src/output/readme.ts
+++ crawler/src/output/readme.ts
@@ -2,7 +2,10 @@ import type { PublishableRepository } from '../storage/repositories.js'
 import { assertValidStatsDraft, type StatsRecord } from './statsDraft.js'
 
 export function tableCell(value: string | null): string {
-  return (value ?? '').replace(/\\/g, '&#92;').replace(/\|/g, '&#124;').replace(/\r\n?|\n/g, ' ')
+  return (value ?? '')
+    .replace(/\\/g, '&#92;')
+    .replace(/\|/g, '&#124;')
+    .replace(/\r\n?|\n/g, ' ')
 }
 
 export function renderReadme(repositories: readonly PublishableRepository[], draft: StatsRecord): string {
